4a0fd9e9e99c8e421c5e77bbcaabbd2f422670de
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-10-22 00:31:28 +0800

    minichat button

--- app/(pages)/(tabs)/layout.tsx
+++ app/(pages)/(tabs)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/global/LeftSidebar";
+import MiniChat from "@/components/pages/messages/minichat/minichat-main";
 
 export default function RootLayout({
   children,
@@ -6,11 +7,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-1 w-full h-full">
+    <section className="relative flex flex-1 w-full h-full">
       <LeftSidebar />
       <article className="flex items-center justify-center flex-[20] bg-slate-100">
         {children}
       </article>
+      <MiniChat />
     </section>
   );
 }
---
+++ components/pages/messages/minichat/minichat-main.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { MessageSquare } from "lucide-react";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const MiniChat = () => {
+  const pathname = usePathname();
+  const messageLink = "/messages";
+  const atMessagePage =
+    (pathname.includes(messageLink) && messageLink.length > 1) ||
+    pathname === messageLink;
+
+  if (atMessagePage) return null;
+
+  return (
+    <Button className="absolute px-4 py-6 text-xl font-bold text-white transition rounded-full shadow bottom-8 right-8 bg-main-500 hover:opacity-40">
+      <span className="">Chat</span>{" "}
+      <MessageSquare className="ml-2 text-white w-7 h-7" />
+    </Button>
+  );
+};
+
+export default MiniChat;
