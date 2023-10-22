6eaffc3c874e3e94749520b5448176ab0deae227
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-10-22 22:35:51 +0800

    Messages Loading Skeleton

--- app/(pages)/(tabs)/messages/layout.tsx
+++ app/(pages)/(tabs)/messages/layout.tsx
@@ -13,18 +13,42 @@ export default function RootLayout({
       <div className="flex">
         <Suspense
           fallback={
-            <div>
-              Loading Message Tabs <Loader2 className="w-5 h-5 animate-spin" />
-            </div>
+            <div className="flex flex-col w-[14rem] py-2 h-screen bg-slate-100 animate-pulse"></div>
           }
         >
           <MessageTabs />
         </Suspense>
         <Suspense
           fallback={
-            <div>
-              Loading Message Drawer
-              <Loader2 className="w-5 h-5 animate-spin" />
+            <div className="flex flex-col w-[20rem] bg-slate-200 animate-pulse">
+              <header className="w-full h-24 px-4 pt-4 pb-6 border-b border-white/30">
+                <div className="relative w-full h-12 bg-white animate-pulse"></div>
+              </header>
+              <main className="flex flex-col flex-1 w-full h-full">
+                <div className="flex items-center justify-center flex-1 w-full overflow-hidden">
+                  <div className="w-full h-[36rem]">
+                    <div className="flex flex-col w-full h-full">
+                      {Array(5)
+                        .fill([])
+                        .map((_, index) => {
+                          return (
+                            <div
+                              key={index}
+                              className={`w-full px-4 py-5 transition bg-slate-100 animate-pulse group relative`}
+                            >
+                              <div className="flex items-center justify-between w-full gap-2">
+                                <div className="w-12 h-12 rounded-full bg-slate-200 animate-pulse" />
+                                <div className="flex flex-col items-center justify-center flex-1 w-full p-1 overflow-hidden text-white">
+                                  <div className="flex items-center justify-between w-full h-12 bg-slate-200 animate-pulse" />
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
+                    </div>
+                  </div>
+                </div>
+              </main>
             </div>
           }
         >
