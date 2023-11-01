06d076c0dba35463550959a8f55bf8364c0aa29c
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-11-01 12:33:59 +0800

    Transactions Page

---
+++ components/pages/transaction/topbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState } from "react";
+
+// UI
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+
+const TransactionTopBar = () => {
+  const TABS = ["All", "Pending", "Paid", "Cancelled"] as const;
+  const [selectedTab, setSelectedTab] = useState("All");
+  return (
+    <header className="flex flex-col justify-center w-full h-32 gap-1 px-4 bg-white">
+      <div className="text-2xl font-bold">Transactions</div>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex gap-4">
+          <div className="relative flex items-center justify-center px-4 border rounded-lg">
+            <Search className="absolute bottom-0 w-5 h-5 -translate-y-1/2 left-2 text-slate-500" />
+            <input
+              placeholder="Search..."
+              className="w-64 h-10 pl-6 border-none outline-none"
+            />
+          </div>
+        </div>
+        <div className="flex p-2 rounded-lg w-fit bg-slate-100">
+          {TABS.map((tab) => {
+            const activeClass =
+              selectedTab === tab
+                ? "bg-white shadow text-black font-semibold rounded-lg"
+                : "";
+            return (
+              <Button
+                key={tab}
+                type="button"
+                onClick={() => setSelectedTab(tab)}
+                variant={"ghost"}
+                className={`text-slate-500 hover:bg-white ${activeClass}`}
+              >
+                {tab}
+              </Button>
+            );
+          })}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default TransactionTopBar;
