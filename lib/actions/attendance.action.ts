4c3e926d4164307772af98fea5e6b51f9e76786e
Ktg8 <113493429+Ktg8@users.noreply.github.com>
2023-11-17 16:17:58 +0800

    Calendar Fixed

--- lib/actions/attendance.action.ts
+++ lib/actions/attendance.action.ts
@@ -7,6 +7,7 @@ import Attendance from "../models/attendance.model";
 import Classes from "../models/class.model";
 import Material from "../models/material.model";
 import Student from "../models/student.model";
+import User from "../models/user.model";
 import connectDB from "../mongodb";
 
 export async function fetchAttendances({
@@ -290,5 +291,82 @@ export async function updateAttendance ({
     return { message: "Student Confirmed Successfully", data: newData };
   } catch (error: any) {
     throw new Error("Error in updating student attendance", error.message);
+  }
+}
+
+export async function fetchAttendanceTeachers({
+  pageNumber = 1,
+  pageSize = 20,
+  currDate,
+}: {
+  pageNumber?: number;
+  pageSize?: number;
+  currDate: Date;
+}) {
+  try {
+    connectDB();
+
+    const presentMonth = new Date();
+    presentMonth.setHours(0, 0, 0, 0);
+
+    const pastMonth = new Date(currDate);
+    pastMonth.setMonth(pastMonth.getMonth() - 1);
+    pastMonth.setHours(0, 0, 0, 0);
+
+    const futureMonth = new Date(currDate);
+    futureMonth.setMonth(futureMonth.getMonth() + 1);
+    futureMonth.setHours(0, 0, 0, 0);
+    // Fetch users with pagination
+    // find greater than past month but less than the future month
+    const query = Attendance.find({
+      date: {
+        // $eq: presentMonth,
+        $gt: pastMonth,
+        $lt: futureMonth,
+      },
+    })
+      .sort({ createdAt: "desc" })
+      .lean()
+      .select("_id date startTime endTime")
+      .populate({
+        path: "class",
+        model: Classes,
+        select: "_id class ageGroup",
+        populate: {
+          path: "participants",
+          model: Student,
+          select: "_id name profileURL",
+        },
+      })
+      .populate({
+        path: "studentsPresent",
+        model: Student,
+      })
+      .exec();
+
+    const totalCount = await User.countDocuments({});
+    const data = await query;
+
+    const plainData = data.map((d: any) => {
+      
+      return {
+        ...d,
+        _id: d._id?.toString(),
+        class: {
+          ...d.class,
+          _id: d.class._id.toString(),
+          participants: d.class.participants.map((d2: any) => {
+            return { ...d2, _id: d2._id.toString() };
+          }),
+        },
+        studentsPresent: d.studentsPresent.map((d2: any) => {
+          return { ...d2, _id: d2._id.toString() };
+        }),
+      };
+    });
+
+    return { attendance: plainData };
+  } catch (error: any) {
+    throw new Error("Error in fetching attendance", error.message);
   }
 }
