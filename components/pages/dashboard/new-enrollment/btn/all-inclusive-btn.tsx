cbcf2ce26f860d0ed581efc013fd0890780b11f8
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-11-24 18:25:45 +0800

    fixed subscription

--- components/pages/dashboard/new-enrollment/btn/all-inclusive-btn.tsx
+++ components/pages/dashboard/new-enrollment/btn/all-inclusive-btn.tsx
@@ -1,14 +1,37 @@
 // @ts-nocheck
 "use client";
-import React from "react";
+import React, { useState } from "react";
+
+// UI
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const AllInclusiveBtn = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <>
-      <stripe-buy-button
-        buy-button-id="buy_btn_1OFCUVJdrjeVG3h14v8ccxp1"
-        publishable-key="pk_live_51JtS02JdrjeVG3h1ComVNr82vGBAXYpUPscimAooqw4YYpXI3UzHKmAYDRgbf37YIZ4GDHsjo0Rw1GZev6htsfG700SGmWjXGT"
-      ></stripe-buy-button>
+      <div className="hidden">
+        <stripe-buy-button
+          buy-button-id="buy_btn_1OFCUVJdrjeVG3h14v8ccxp1"
+          publishable-key="pk_live_51JtS02JdrjeVG3h1ComVNr82vGBAXYpUPscimAooqw4YYpXI3UzHKmAYDRgbf37YIZ4GDHsjo0Rw1GZev6htsfG700SGmWjXGT"
+        ></stripe-buy-button>
+      </div>
+      <Button
+        disabled={isLoading}
+        type="button"
+        className="w-full py-6 text-base font-bold"
+        onClick={() => {
+          setIsLoading(true);
+          window.open(
+            "https://checkout.umonicsplus.com/b/aEUdUg1275PxfCw9AC?locale=en&__embed_source=buy_btn_1OFCUVJdrjeVG3h14v8ccxp1",
+            "_blank"
+          );
+        }}
+      >
+        Enroll Now
+        {isLoading && <Loader2 className="w-6 h-6 ml-2 animate-spin" />}
+      </Button>
     </>
   );
 };
--- components/pages/dashboard/new-enrollment/btn/basic-btn.tsx
+++ components/pages/dashboard/new-enrollment/btn/basic-btn.tsx
@@ -1,14 +1,37 @@
 // @ts-nocheck
 "use client";
-import React from "react";
+import React, { useState } from "react";
+
+// UI
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const LessonsOnlyBtn = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <>
-      <stripe-buy-button
-        buy-button-id="buy_btn_1OFCYgJdrjeVG3h12HCb9Z3C"
-        publishable-key="pk_live_51JtS02JdrjeVG3h1ComVNr82vGBAXYpUPscimAooqw4YYpXI3UzHKmAYDRgbf37YIZ4GDHsjo0Rw1GZev6htsfG700SGmWjXGT"
-      ></stripe-buy-button>
+      <div className="hidden">
+        <stripe-buy-button
+          buy-button-id="buy_btn_1OFCYgJdrjeVG3h12HCb9Z3C"
+          publishable-key="pk_live_51JtS02JdrjeVG3h1ComVNr82vGBAXYpUPscimAooqw4YYpXI3UzHKmAYDRgbf37YIZ4GDHsjo0Rw1GZev6htsfG700SGmWjXGT"
+        ></stripe-buy-button>
+      </div>
+      <Button
+        disabled={isLoading}
+        type="button"
+        className="w-full py-6 text-base font-bold"
+        onClick={() => {
+          setIsLoading(true);
+          window.open(
+            "https://checkout.umonicsplus.com/b/8wMdUgbGL6TB4XS003?locale=en&__embed_source=buy_btn_1OFCYgJdrjeVG3h12HCb9Z3C",
+            "_blank"
+          );
+        }}
+      >
+        Enroll Now
+        {isLoading && <Loader2 className="w-6 h-6 ml-2 animate-spin" />}
+      </Button>
     </>
   );
 };
