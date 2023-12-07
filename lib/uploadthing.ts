27674b826e31e65751fa1200df09f8a44c9dafbf
Ktg8 <113493429+Ktg8@users.noreply.github.com>
2023-12-07 13:54:23 +0800

    UploadButton error in deployment fix

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,10 @@
 "use client";
 import { OurFileRouter } from "@/app/(routes)/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react/hooks";
+import { generateComponents } from "@uploadthing/react";
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
+
+export const { UploadButton, UploadDropzone, Uploader } =
+  generateComponents<OurFileRouter>();
